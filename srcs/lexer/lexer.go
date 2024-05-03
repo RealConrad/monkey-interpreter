e9d3b225d7eeb4308e01b27d86ba0f438920ecc6
RealConrad <wenzconrad@gmail.com>
2024-05-03 23:11:54 +0200

    added basic lexer with basic test

--- srcs/lexer/lexer.go
+++ srcs/lexer/lexer.go
@@ -1,8 +1,6 @@
 package lexer
 
 import (
-	"testing"
-
 	"github.com/RealConrad/monkey-interpreter/srcs/token"
 )
 
@@ -13,37 +11,49 @@ type Lexer struct {
 	ch			byte // the char we testing
 }
 
+func (lexer *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	lexer.readChar()
+	switch lexer.ch {
+		case '=':
+			tok = newToken(token.ASSIGN, lexer.ch)
+		case ';':
+			tok = newToken(token.SEMICOLON, lexer.ch)
+		case '(':
+			tok = newToken(token.LPAREN, lexer.ch)
+		case ')':
+			tok = newToken(token.RPAREN, lexer.ch)
+		case ',':
+			tok = newToken(token.COMMA, lexer.ch)
+		case '+':
+			tok = newToken(token.PLUS, lexer.ch)
+		case '{':
+			tok = newToken(token.LBRACE, lexer.ch)
+		case '}':
+			tok = newToken(token.RBRACE, lexer.ch)
+		case 0:
+			tok.Literal = ""
+			tok.Type = token.EOF
+	}
+	return tok
+}
+
 func New(input string) *Lexer {
 	lex := &Lexer{input: input}
 	return lex
 }
 
-func TestNextToken(t *testing.T) {
-	input := "=+(){};"
-
-	tests := []struct {
-		expectedType	token.TokenType
-		expectedLiteral	string
-	}{
-		{token.ASSIGN, "="},
-		{token.PLUS, "+"},
-		{token.LPAREN, "("},
-		{token.RPAREN, ")"},
-		{token.LBRACE, "{"},
-		{token.RBRACE, "}"},
-		{token.COMMA, ","},
-		{token.SEMICOLON, ";"},
-		{token.EOF, ""},
+func (lexer *Lexer) readChar() {
+	if lexer.readPostion >= len(lexer.input) {
+		lexer.ch = 0
+	} else {
+		lexer.ch = lexer.input[lexer.readPostion]
 	}
+	lexer.position = lexer.readPostion
+	lexer.readPostion++;
+}
 
-	for i, tokenType := range tests {
-		tok := l.NextToken()
-
-		if tok.Type != tokenType.expectedType {
-			t.Fatal("tests[%d] - token type is wrong. expected=%q, got=%q", i, tokenType.expectedLiteral, tok.Type)
-		}
-		if tok.Literal != tokenType.expectedLiteral {
-			t.Fatalf("tests[%d] - literal wrong. expected=%q, got=%q", i, tokenType.expectedLiteral, tok.Literal)
-		}
-	}
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
