79b1de82633f48a83ffdcb1ed2f068a8e3cd0294
RealConrad <wenzconrad@gmail.com>
2024-05-08 22:18:39 +0200

    added basic AST types

---
+++ srcs/ast/ast.go
@@ -0,0 +1,58 @@
+package ast
+
+import "github.com/RealConrad/monkey-interpreter/srcs/token"
+
+type Node interface {
+	TokenLiteral() string
+}
+
+type Statement interface {
+	Node
+	statementNode()
+}
+
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+type Program struct {
+	Statements []Statement
+}
+
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name *Identifier
+	Value Expression
+}
+
+/* --------------------------- LET-STATEMENT FUNC --------------------------- */
+
+func (ls *LetStatement) statementNode() {}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+/* ----------------------------- IDENTIFIER FUNC ---------------------------- */
+
+func (i *Identifier) expressionNode() {}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
+
+/* ------------------------------ PROGRAM FUNC ------------------------------ */
+
+func (p *Program) TokenLiteral() string {
+	if (len(p.Statements) > 0) {
+		return p.Statements[0].TokenLiteral()
+	}
+	return ""
+}
+
