ae0b6b134fbb12600a850e3fd457d30cef0189f1
RealConrad <wenzconrad@gmail.com>
2024-06-10 22:15:49 +0200

    added error handling

--- srcs/parser/parser.go
+++ srcs/parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
 import (
+	"fmt"
+
 	"github.com/RealConrad/monkey-interpreter/srcs/ast"
 	"github.com/RealConrad/monkey-interpreter/srcs/lexer"
 	"github.com/RealConrad/monkey-interpreter/srcs/token"
@@ -8,13 +10,16 @@ import (
 
 type Parser struct {
 	l *lexer.Lexer
-
 	curToken token.Token
 	peekToken token.Token
+	errors []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l: l,
+		errors: []string{},
+	}
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
 	p.nextToken()
@@ -33,7 +38,18 @@ func (p *Parser) parseStatement() ast.Statement {
 		default:
 			return nil
 	}
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
 }	
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s instead",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
+}
+	
 
 func (p *Parser) ParseProgram() *ast.Program {
 	// construct the root node of the AST
@@ -80,6 +96,8 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
 	if p.peekTokenIs(t) {
 		p.nextToken()
 		return true
+	} else {
+		p.peekError(t)
+		return false
 	}
-	return false
 }
